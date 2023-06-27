df14a74a6da4592a05a7102760b6731741005eb4
Fouzia <fouzia.hollande@gmail.com>
2023-06-27 22:40:57 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <NavBar />
+
+  <RouterView/>
+</template>
+
+<script setup>
+
+/*
+  imports
+*/
+
+  import  NavBar  from "@/components/Layout/NavBar.vue";
+</script>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+</style>
+
